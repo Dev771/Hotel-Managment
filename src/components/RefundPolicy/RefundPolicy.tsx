b1d9b58ab085619f0e16e401c1d46113cb948a29
Dev Garg <devgarg771@gmail.com>
2025-01-12 12:23:31 +0530

    Update

--- src/components/RefundPolicy/RefundPolicy.tsx
+++ src/components/RefundPolicy/RefundPolicy.tsx
@@ -1,8 +1,37 @@
 
-const RefundPolicy = () => {
-  return (
-    <div>RefundPolicy</div>
-  )
-}
+const RefundPolicy = () => (
+  <div className='page-wrapper'>
+    <section className="featured-section-four about-page" style={{ paddingBottom: '60px' }}>
+        <span className="dotted-pattern dotted-pattern-11"></span>
+        <div className="circles-two">
+            <div className="c-1"></div>
+            <div className="c-2"></div>
+        </div>
+        <div className="auto-container">
+            <div className="row clearfix">
+                <div className="text-column col-lg-12 col-md-12 col-sm-12">
+                    <div className="inner">
+                        <div className="sec-title">
+                            <h2 className="text-center">Refund & Cancellation Policy</h2>
+                        </div>
+                        <div className="text">
+                            <ul>
+                                <li>Room tariff is subject to tax as per revealing laws. Taxes are subject to change as per govt. regulations without prior intimations. Current Tax structure: 12% GST for Tariff of INR 1001 to INR 7500.</li>
+                                <li>Hotel’s Check in time is 13:00 hrs and Check out time is 11:00 hrs, early check in and late check-out request can be considered subject to be availibilty of room with an addtional charges. An Early departure would liable to pay room night charges for the entire booking period.</li>
+                                <li>All the guaranteed reservations are non refundable.</li>
+                                <li>Modified bookings will be subject to availibility and revised booking policy of the Hotel.</li>
+                                <li>The Cancellation/modification charges are standard and any waiver is on the Hotel’s discretion.</li>
+                                <li>The inclusion of Extra bed with a booking is facilitated with a mattress as an extra bed.</li>
+                                <li>Rooms will be held on confirm basis subject to guarantee by advance payment via NEFT, Bank Account Credit Card or Cash payment.</li>
+                                <li>Only credit-note facility available for the refund/deposit amount.</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+  </div>
+)
 
 export default RefundPolicy
